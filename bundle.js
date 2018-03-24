e60d8181dd8dbef37ff7bc7bb85fa4d82c627d5e
TTNguyen <ttn210@d-ip-10-194-67-159.ouguest.nor.ou.edu>
2018-03-24 12:37:37 -0500

    More Week4

--- bundle.js
+++ bundle.js
@@ -2,11 +2,34 @@
 var $ = require("jquery");
 var _ = require("lodash");
 
-var list = ["tom", "braxton", "whitney", "tanner" ];
+var list = [ "tom", "braxton", "whitney", "tanner" ];
+
+function addlist(){
+ var newItem = $("#newItem").val();
+ list.push(newItem);
+ printList();
+ $("#newItem").val("");
+}
 
 function message(){
 	console.log("This is a message");
+	alert("This is a message");
 }
+
+function printList(){
+	var ourlist = $("#nameList");
+	ourlist.html("");
+	_(list).forEach(function(el){
+	 ourlist.append("<li>" + el + "</li>");
+	});
+}
+
+$(document).ready(printList);
+
+window.list = list;
+window.addlist = addlist;
+window.printList = printList;
+
 },{"jquery":2,"lodash":3}],2:[function(require,module,exports){
 /*!
  * jQuery JavaScript Library v3.3.1
--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 var $ = require("jquery");
 var _ = require("lodash");
 
-var list = ["tom", "braxton", "whitney", "tanner" ];
+var list = [ "tom", "braxton", "whitney", "tanner" ];
+
+function addlist(){
+ var newItem = $("#newItem").val();
+ list.push(newItem);
+ printList();
+ $("#newItem").val("");
+}
 
 function message(){
 	console.log("This is a message");
 	alert("This is a message");
 }
 
-$(document).ready(function(){
-	$("p").html("<div>Another Message</div>");
-});
+function printList(){
+	var ourlist = $("#nameList");
+	ourlist.html("");
+	_(list).forEach(function(el){
+	 ourlist.append("<li>" + el + "</li>");
+	});
+}
 
-window.message = message;
+$(document).ready(printList);
+
+window.list = list;
+window.addlist = addlist;
+window.printList = printList;
